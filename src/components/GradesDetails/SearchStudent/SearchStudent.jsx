6129c4ecf16e68e3657e5e5ab3f854bc695eac72
Dylan <dcren2010@gmail.com>
2021-07-12 19:23:22 -0400

    update search bar

--- src/components/GradesDetails/SearchStudent/SearchStudent.jsx
+++ src/components/GradesDetails/SearchStudent/SearchStudent.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from './SearchStudent.module.css'
 
 const SearchStudent = ({setSearchField}) => {
 	// Search Function:
@@ -7,7 +8,7 @@ const SearchStudent = ({setSearchField}) => {
 	};
 
 	return (
-		<div className='searchBar'>
+		<div className={styles.searchBar}>
 			<input onChange={(e) => searchStudents(e)} type='search' placeholder='Search by name' />
 		</div>
 	);
